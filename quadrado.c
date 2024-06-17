aa4b6f5a4de8b866b4d1847737bc720870c1d232
Felipe Costa Unsonst <161458493+felipeunsonst@users.noreply.github.com>
2024-06-17 20:22:05 -0300

    Update quadrado.c

--- quadrado.c
+++ quadrado.c
@@ -1,3 +1,14 @@
+/*Faça um programa que leia um inteiro positivo qualquer e um caractere que indica o tipo de quadrado (de lado l) a ser impresso na tela utilizando o caractere '*'. Se o caractere for:
+
+'c', imprima um quadrado totalmente preenchido;
+'b', imprima somente suas bordas;
+'p', imprima suas bordas e a diagonal principal;
+'s', imprima suas bordas e a diagonal secundária;
+'h', imprima suas bordas e sua divisão horizontal (no meio);
+'v' imprima suas bordas e sua divisão vertical (no meio);
+caso contrário, imprima a mensagem de erro: "Não existe essa opção".
+RESTRIÇÕES: Não se pode utilizar nenhum conceito não visto em sala de aula (por ex., strings)
+*/
 #include <stdio.h>
 
 int main(){
